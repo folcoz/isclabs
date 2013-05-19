1570281f0aa43ee2df34888e2cbbf8be249f6606
folcoz <folcoz@yahoo.es>
2013-05-19 12:56:21 +0200

    Support linking to a lab and pressing Refresh button.

--- app/main.js
+++ app/main.js
@@ -1,4 +1,5 @@
-/*global require, isc */
+/*global require, isc, APPINIT */
+
 require(['i18n', 'base/all', 'presenter/presenters', 'labs/labs', 'labs/widgets'], function (i18n, base, presenters, labs, widgets) {
     'use strict';
 
@@ -21,11 +22,21 @@ require(['i18n', 'base/all', 'presenter/presenters', 'labs/labs', 'labs/widgets'
 
     isc.History.registerCallback({target: historyPlayback, method: historyPlayback.fn});
 
+    // Comprueba si hay que navegar a uno de los laboratorios porque ha sido
+    // especificado en la URL
+    function navigateToLab() {
+        var fragment = APPINIT.utils.getFragment();
+        if (fragment) {
+            historyPlayback.fn(fragment, null);
+        }
+    }
+
     var runMainPresenter = function (data) {
         var mainPresenter = presenters.Main.create({
             userinfo: data
         });
         mainPresenter.run();
+        navigateToLab();
     };
 
     base.config.asyncUserInfo().then(runMainPresenter);
